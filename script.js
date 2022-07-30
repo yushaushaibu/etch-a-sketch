435e5789e30f7b21f66d749f9a6001b9c54892aa
Yushau Shaibu <yushaushaibu1@gmail.com>
2022-07-30 22:15:02 +0000

    updated html, and script file.

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 const grid = document.getElementById('grid');
 const colorPicker = document.getElementById('colorPicker');
-const colorMode = document.getElementById('colorMode');
-const rainbowMode = document.getElementById('rainbowMode');
-const eraser = document.getElementById('eraser');
-const clear = document.getElementById('clear');
+let colorBtn = document.getElementById('colorBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const eraserBtn = document.getElementById('eraserBtn');
+const clearBtn = document.getElementById('clearBtn');
+const sizeValue = document.getElementById('sizeValue');
 const sizeSlider = document.getElementById('sizeSlider');
-console.log(sizeSlider);
-
-
-// for(let i = 0; i <= 256; i++) {
-//     const item = document.createElement('div');
-//     item.classList.add("item", "item " + i);
-//     grid.appendChild(item);
-// }
 
 function makeCells(rows, cols) {
     grid.style.setProperty('--grid-rows', rows);
@@ -23,5 +16,42 @@ function makeCells(rows, cols) {
       grid.appendChild(cell).className = "grid-item";
     };
   };
-  
+
   makeCells(16, 16);
+
+
+const DEFAULT_COLOR = '#1A1A47';
+const DEFAULT_MODE = 'color';
+const DEFAULT_SLIDER_SIZE = 16;
+
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SLIDER_SIZE;
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
+
+function setCurrentMode(newMode) {
+    activateButton(newMode)
+    currentMode = newMode
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode('color');
+rainbowBtn.onclick = () => setCurrentMode('rainbow');
+eraserBtn.onclick = () => setCurrentMode('eraser');
+clearBtn.onclick = () => reloadGrid()
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+// colorPicker.onclick(function(e) {
+//     return e.target.value;
+// })
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
